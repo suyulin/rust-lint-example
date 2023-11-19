b4c7995c88ef99a06ffa4759924a42ecc1d1d24f
不求圣剑 <suyulingxm@hotmail.com>
2023-11-19 19:05:01 +0800

    feat: add sub fun

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     println!("Hello, world!");
     println!("1 + 2 = {}", add(1, 2));
+    println!("1 - 2 = {}", sub(1, 2));
 }
-
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
 fn add(a: i32, b: i32) -> i32 {
     a + b
 }
@@ -11,4 +14,9 @@ fn add(a: i32, b: i32) -> i32 {
 #[test]
 fn test_add() {
     assert_eq!(add(1, 2), 3);
+}
+// test sub
+#[test]
+fn test_sub() {
+    assert_eq!(sub(1, 2), -1);
 }
